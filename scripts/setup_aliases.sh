f437a65db89135ce9740a6e953b6b44264177b71
Drew White <32739358+Drucial@users.noreply.github.com>
2024-05-26 14:46:54 -0400

    setup aliases and start on icon

---
+++ scripts/setup_aliases.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# Aliases for Hyperdirmic development scripts
+alias run_hyperdirmic_dev="$(pwd)/scripts/run_dev.sh"
+alias run_hyperdirmic="$(pwd)/scripts/run.sh"
+alias test_hyperdirmic="$(pwd)/scripts/test.sh"
+alias lint_hyperdirmic="$(pwd)/scripts/lint.sh"
+alias format_hyperdirmic="$(pwd)/scripts/format.sh"
+alias activate_hyperdirmic_env="source $(pwd)/venv/bin/activate"
+
+echo "Aliases for Hyperdirmic development scripts have been set."
+
